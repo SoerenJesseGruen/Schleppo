932dc2a397d8155e02743e6d92a7d94486bbea41
SoerenJesseGruen <soeren.jesse@gmx.de>
2016-11-23 14:59:30 +0100

    Fragment-Layouts zum Test angepasst, Main-Fragment fertiggstellt

--- app/src/main/java/moco/schleppo/fragments/MainFragment.java
+++ app/src/main/java/moco/schleppo/fragments/MainFragment.java
@@ -1,10 +1,12 @@
 package moco.schleppo.fragments;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import moco.schleppo.R;
 
@@ -16,7 +18,42 @@ public class MainFragment extends Fragment {
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        setClickListener(rootView);
 
         return rootView;
+    }
+
+    private void setClickListener (View rootView) {
+
+        ImageButton btnProfile = (ImageButton) rootView.findViewById(R.id.imageButton_Profile);
+        ImageButton btnMap = (ImageButton) rootView.findViewById(R.id.imageButton_Map);
+        ImageButton btnMessages = (ImageButton) rootView.findViewById(R.id.imageButton_Messages);
+        ImageButton btnWarnDriver = (ImageButton) rootView.findViewById(R.id.imageButton_Warning);
+
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                FragmentManager fm = getFragmentManager();
+                fm.beginTransaction().replace(R.id.content_frame, new ProfilFragment()).commit();
+            }
+        });
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                FragmentManager fm = getFragmentManager();
+                fm.beginTransaction().replace(R.id.content_frame, new MainFragment()).commit();
+            }
+        });
+        btnMessages.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                FragmentManager fm = getFragmentManager();
+                fm.beginTransaction().replace(R.id.content_frame, new MessagesFragment()).commit();
+            }
+        });
+        btnWarnDriver.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                FragmentManager fm = getFragmentManager();
+                fm.beginTransaction().replace(R.id.content_frame, new WarnDriverFragment()).commit();
+            }
+        });
+
     }
 }
