1f79fb382da71fcddd79417ec3959edef293395d
SoerenJesseGruen <soeren.jesse@gmx.de>
2016-11-23 14:12:20 +0100

    Initial commit

---
+++ app/src/main/java/moco/schleppo/fragments/LogoutFragment.java
@@ -0,0 +1,22 @@
+package moco.schleppo.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import moco.schleppo.R;
+
+/**
+ * Created by soere on 23.11.2016.
+ */
+
+public class LogoutFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return rootView;
+    }
+}
